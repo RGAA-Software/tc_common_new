4bbe5c9da622057a7138c63055b41d48e6e547ef
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-02-16 16:49:14 +0800

    start process...
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- process_util.cpp
+++ process_util.cpp
@@ -4,6 +4,8 @@
 
 #include "process_util.h"
 
+#include "tc_common/log.h"
+
 namespace tc {
 #ifdef WIN32
     bool SetDpiAwarenessContext(DPI_AWARENESS_CONTEXT context) {
@@ -25,4 +27,17 @@ namespace tc {
         return res;
     }
 #endif
+
+
+    bool ProcessUtil::StartProcess(const std::string& exe_path, const std::vector<std::string>& args) {
+        bp::ipstream pipe_stream;
+        bp::child c(exe_path, bp::args(args), bp::std_out > pipe_stream);
+        c.wait();
+        int code = c.exit_code();
+        if (code != 0) {
+            LOGE("Start process code : {}", code);
+        }
+        return code == 0;
+    }
+
 }
--- process_util.h
+++ process_util.h
@@ -6,15 +6,26 @@
 #define TC_APPLICATION_PROCESS_UTIL_H
 
 #ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #endif
+
+#include <boost/process.hpp>
+
+namespace bp = boost::process;
 
 namespace tc {
 #ifdef WIN32
     bool SetDpiAwarenessContext(DPI_AWARENESS_CONTEXT context);
-
-
 #endif
+
+    class ProcessUtil {
+    public:
+
+        static bool StartProcess(const std::string& exe_path, const std::vector<std::string>& args);
+
+    };
+
 }
 
 #endif //TC_APPLICATION_PROCESS_UTIL_H
