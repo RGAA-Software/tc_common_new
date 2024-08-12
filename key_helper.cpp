656972f6545ce45b5844cb1ea1bb00fe1f8e8532
hy <hy@gmail.com>
2024-08-12 11:18:57 +0800

    fix building on android platform
    Signed-off-by: hy <hy@gmail.com>

--- key_helper.cpp
+++ key_helper.cpp
@@ -20,27 +20,51 @@ namespace tc
     }
 
     bool KeyHelper::IsShiftPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_LSHIFT) || IsKeyPressed(VK_RSHIFT);
+#else
+        return false;
+#endif
     }
 
     bool KeyHelper::IsControlPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_LCONTROL) || IsKeyPressed(VK_RCONTROL);
+#else
+        return false;
+#endif
     }
 
     bool KeyHelper::IsAltPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_LMENU) || IsKeyPressed(VK_RMENU);
+#else
+        return false;
+#endif
     }
 
     bool KeyHelper::IsWinPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_LWIN) || IsKeyPressed(VK_RWIN);
+#else
+        return false;
+#endif
     }
 
     bool KeyHelper::IsCapsLockPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_CAPITAL);
+#else
+        return false;
+#endif
     }
 
     bool KeyHelper::IsNumLockPressed() {
+#ifdef WIN32
         return IsKeyPressed(VK_NUMLOCK);
+#else
+        return false;
+#endif
     }
 
     int KeyHelper::GetKeyStateInner(int vk) {
@@ -52,11 +76,19 @@ namespace tc
     }
 
     int KeyHelper::GetCapsLockState() {
+#ifdef WIN32
         return KeyHelper::GetKeyStateInner(VK_CAPITAL);
+#else
+        return -22;
+#endif
     }
 
     int KeyHelper::GetNumLockState() {
+#ifdef WIN32
         return GetKeyState(VK_NUMLOCK);
+#else
+        return -22;
+#endif
     }
 
 }
