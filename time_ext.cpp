bf78454f63b9f98f9ea41f0ccf0a6cc1b132d2c5
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-03-17 10:15:12 +0800

    add time duration
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- time_ext.cpp
+++ time_ext.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "time_ext.h"
+#include "log.h"
+
+namespace tc
+{
+
+    TimeDuration::TimeDuration(const std::string& name) {
+        name_ = name;
+        begin_ts_ = TimeExt::GetCurrentTimestamp();
+    }
+
+    TimeDuration::~TimeDuration() {
+        auto end_ts_ = TimeExt::GetCurrentTimestamp();
+        LOGI("[{}] used {}ms", name_, (end_ts_ - begin_ts_));
+    }
+
+}
