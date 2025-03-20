a4470a9fe2418963c8f32bc4ed5bcc406458256d
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-03-20 22:50:03 +0800

    modify qr generator
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- qrcode/qr_generator.cpp
+++ qrcode/qr_generator.cpp
@@ -4,6 +4,7 @@
 
 #include "qr_generator.h"
 #include "qrcodegen.hpp"
+#include "tc_common_new/log.h"
 
 using namespace qrcodegen;
 
@@ -16,16 +17,20 @@ namespace tc
         QrCode qr1 = QrCode::encodeSegments(segs, QrCode::Ecc::LOW, 10, 10, 1, false);
         QImage QrCode_Image=QImage(qr1.getSize(),qr1.getSize(),QImage::Format_RGB888);
         QrCode_Image.fill(Qt::transparent);
+        LOGI("QRCode, size: {}", qr1.getSize());
         for (int y = 0; y < qr1.getSize(); y++) {
             for (int x = 0; x < qr1.getSize(); x++) {
                 if(qr1.getModule(x, y)) {
                     QrCode_Image.setPixel(x, y, qRgb(0, 0, 0));
+                    //QrCode_Image.setPixel(x, y, qRgb(200-x*5+y*x, 100+x*y, x+10*y-2));
                 } else {
                     QrCode_Image.setPixel(x, y, qRgb(255, 255, 255));
                 }
             }
         }
-        QrCode_Image=QrCode_Image.scaled(qr_size, qr_size, Qt::KeepAspectRatio);
+        if (qr_size != -1) {
+            QrCode_Image = QrCode_Image.scaled(qr_size, qr_size, Qt::KeepAspectRatio);
+        }
         return QPixmap::fromImage(QrCode_Image);
     }
 
