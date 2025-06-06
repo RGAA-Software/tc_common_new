1ed9ac61f8d6635768ea67a2de4e49d3a8f79e69
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-06-06 12:41:49 +0800

    add client id extractor
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ client_id_extractor.h
@@ -0,0 +1,27 @@
+//
+// Created by RGAA on 6/06/2025.
+//
+
+#ifndef GAMMARAY_CLIENT_ID_EXTRACTOR_H
+#define GAMMARAY_CLIENT_ID_EXTRACTOR_H
+
+#include <vector>
+#include "string_ext.h"
+
+namespace tc
+{
+
+    // client_702789003_d41d8cd98f00b204e9800998ecf8427e
+    // ==> 702789003
+    static std::string ExtractClientId(const std::string& full_id) {
+        std::vector<std::string> result;
+        StringExt::Split(full_id, result, "_");
+        if (result.size() > 1) {
+            return result[1];
+        }
+        return full_id;
+    }
+
+}
+
+#endif //GAMMARAY_CLIENT_ID_EXTRACTOR_H
