b06934ba80bb64306fab71083c1b4b1ac1ed10c8
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-06-26 15:34:39 +0800

    add a method to start process in current console user
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- process_util.h
+++ process_util.h
@@ -22,7 +22,10 @@ namespace tc
         static std::vector<std::string> StartProcessAndOutput(const std::string& exe_path, const std::vector<std::string>& args);
         static bool StartProcessInWorkDir(const std::string& work_dir, const std::string& cmdline, const std::vector<std::string>& args);
         static bool KillProcess(unsigned long pid);
-        static bool StartProcessAsUser(const std::wstring& cmdline, const std::wstring& work_dir, bool wait);
+        // Will run in the same user with the executor
+        static bool StartProcessInSameUser(const std::wstring& cmdline, const std::wstring& work_dir, bool wait);
+        // Will run in current user
+        static bool StartProcessInCurrentUser(const std::wstring& cmdline, const std::wstring& work_dir, bool wait);
         static uint32_t GetCurrentSessionId();
         static uint32_t GetProcessSessionId(uint32_t pid);
         static int GetThreadCount();
