43b4c56f60d86a8b6307483491688ea0e963f625
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-07-06 01:01:01 +0800

    add concurrent type
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ concurrent_type.h
@@ -0,0 +1,56 @@
+//
+// Created by RGAA on 6/07/2025.
+//
+
+#ifndef GAMMARAY_CONCURRENT_TYPE_H
+#define GAMMARAY_CONCURRENT_TYPE_H
+
+#include <mutex>
+#include <string>
+
+namespace tc
+{
+    template<typename T>
+    class ConcurrentType {
+    public:
+        ConcurrentType() {
+
+        }
+
+        ConcurrentType(const ConcurrentType& other) {
+            T other_value = other.Load();
+            this->Update(other_value);
+        }
+
+        void Update(const T& s) {
+            std::lock_guard<std::mutex> guard(mtx_);
+            inner_ = s;
+        }
+
+        T Load() const {
+            std::lock_guard<std::mutex> guard(mtx_);
+            return inner_;
+        }
+
+        ConcurrentType<T>& operator=(const ConcurrentType<T>& other) {
+            if (this != &other) {
+                T other_value = other.Load();
+                this->Update(other_value);
+            }
+            return *this;
+        }
+
+        ConcurrentType<T>& operator=(const T& other) {
+            this->Update(other);
+            return *this;
+        }
+
+    private:
+        mutable std::mutex mtx_;
+        T inner_;
+    };
+
+    using ConcurrentString = tc::ConcurrentType<std::string>;
+}
+
+#endif //GAMMARAY_CONCURRENT_TYPE_H
