09a92155663e086a044fe268ad9490c856f4edeb
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-07-28 17:43:08 +0800

    add auto start
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ auto_start.h
@@ -0,0 +1,51 @@
+//
+// Created by RGAA on 28/07/2025.
+//
+
+#ifndef GAMMARAYPREMIUM_AUTO_START_H
+#define GAMMARAYPREMIUM_AUTO_START_H
+
+#include <QString>
+#include <taskschd.h>
+#pragma comment(lib, "taskschd.lib")
+
+namespace tc
+{
+
+    class AutoStart {
+    public:
+        // reg
+        static void SetAutoStart(const QString& exe_path, bool enabled);
+        static void SetAutoStartAdmin(const QString& exe_path, bool enabled);
+
+        AutoStart();
+        ~AutoStart();
+
+        // task
+        //************************************
+        // 函数名:  CMyTaskSchedule::NewTask
+        // 返回类型:   BOOL
+        // 功能: 创建计划任务
+        // 参数1: char * lpszTaskName    计划任务名
+        // 参数2: char * lpszProgramPath    计划任务路径
+        // 参数3: char * lpszParameters        计划任务参数
+        // 参数4: char * lpszAuthor            计划任务作者
+        //************************************
+        BOOL NewTask(char* lpszTaskName, char* lpszProgramPath, char* lpszParameters, char* lpszAuthor);
+
+        //************************************
+        // 函数名:  CMyTaskSchedule::Delete
+        // 返回类型:   BOOL
+        // 功能: 删除计划任务
+        // 参数1: char * lpszTaskName    计划任务名
+        //************************************
+        BOOL Delete(char* lpszTaskName);
+
+    private:
+        ITaskService *m_lpITS = nullptr;
+        ITaskFolder *m_lpRootFolder = nullptr;
+    };
+
+}
+
+#endif //GAMMARAYPREMIUM_AUTO_START_H
