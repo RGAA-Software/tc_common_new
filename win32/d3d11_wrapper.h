df3937b9460a2b95c649810610a9927a3655f3da
Hector <hollow.ichigo.yoo@gmail.com>
2025-09-04 17:49:19 +0800

    add d3d11 wrapper
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

---
+++ win32/d3d11_wrapper.h
@@ -0,0 +1,39 @@
+//
+// Created by RGAA on 1/05/2025.
+//
+
+#ifndef GAMMARAY_D3D11_WRAPPER_H
+#define GAMMARAY_D3D11_WRAPPER_H
+#ifdef WIN32
+
+#include <memory>
+#include <d3d11.h>
+#include <wrl/client.h>
+
+using namespace Microsoft::WRL;
+
+namespace tc
+{
+
+    class D3D11DeviceWrapper {
+    public:
+        void Release() const {
+            if (d3d11_device_context_) {
+                d3d11_device_context_->ClearState();
+                d3d11_device_context_->Flush();
+                d3d11_device_context_->Release();
+            }
+            if (d3d11_device_) {
+                d3d11_device_->Release();
+            }
+        }
+
+    public:
+        uint64_t adapter_uid_ = 0;
+        ComPtr<ID3D11Device> d3d11_device_ = nullptr;
+        ComPtr<ID3D11DeviceContext> d3d11_device_context_ = nullptr;
+    };
+
+}
+#endif
+#endif //GAMMARAY_D3D11_WRAPPER_H
