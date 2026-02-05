f4ef26e6d0d3c2cc6290597a520ac8cc7e97a33f
Hector <hollow.ichigo.yoo@gmail.com>
2026-02-05 13:30:43 +0800

    update const auto
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

---
+++ const_auto.h
@@ -0,0 +1,10 @@
+//
+// Created by RGAA on 5/02/2026.
+//
+
+#ifndef GAMMARAYPREMIUM_CONST_AUTO_H
+#define GAMMARAYPREMIUM_CONST_AUTO_H
+
+#define cat const auto
+
+#endif //GAMMARAYPREMIUM_CONST_AUTO_H
